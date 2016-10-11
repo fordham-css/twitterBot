799d3021c89cf8baf59f7c3f5071d5a791b5bf47
Ian Granger <icgranger9@gmail.com>
2016-10-11 17:38:24 -0400

    add auth_example.py

--- auth_example.py
+++ auth_example.py
@@ -1,8 +1,8 @@
 # key info for python twitter bot
 # used so the keys won't be seen on GitHub
 
-Consumer_Key = ""
-Consumer_Secret = ""
+Consumer_Key = " "
+Consumer_Secret = " "
 
-Access_Token = ""
-Access_Secret = ""
+Access_Token = " "
+Access_Secret = " "
