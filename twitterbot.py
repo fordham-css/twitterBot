d30205625d1e8660ec07270c20a333cd19c066b8
icgranger9 <icgranger9@gmail.com>
2016-10-11 16:37:05 -0400

    Add Basic twitterbot

---
+++ twitterbot.py
@@ -0,0 +1,18 @@
+#requires python-twitter
+	#pip install python-twitter
+import twitter
+
+#requires auth.py, which holds all keys and tokens
+from auth import *
+
+#uses the keys to connect to the twitter API
+api = twitter.Api(consumer_key= Consumer_Key,
+					consumer_secret= Consumer_Secret,
+					access_token_key= Access_Token,
+					access_token_secret= Access_Secret)
+
+#Posts the update
+status = api.PostUpdate("My First Bot Post!")
+
+#prints the update to the screen
+print(status.text)
